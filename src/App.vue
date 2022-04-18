af3528dd9966d5a99e9932804a0f445a7c691ba4
hanna labushkina <hanna.labushkina@reedonline.co.uk>
2022-04-19 02:03:52 +0400

    first try buildable projects create App and first component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello</h1>
+  <Counter />
+  <hr/>
+  <Counter />
+</template>
+
+<script>
+import Counter from "./Counter.vue";
+
+export default {
+  modules: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <button v-on:click="handleClick(btnValue-1)">-</button>
+  <input v-model="btnValue"/>
+  <button v-on:click="handleClick(btnValue+1)">+</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  methods: {
+    handleClick(value) {
+      this.btnValue = value
+    }
+  },
+  data() {
+    return {
+      btnValue: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
